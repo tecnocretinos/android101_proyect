d0fcc27c65ebacbc8fad24e2e8b8fcd07870ea33
Tiago Moreno <santiago.moreno@astropay.com>
2022-06-22 22:11:41 -0500

    Signup and recovery activities

---
+++ app/src/main/java/tech/yeswecode/reporteciudadano/RecoverPasswordActivity.kt
@@ -0,0 +1,11 @@
+package tech.yeswecode.reporteciudadano
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RecoverPasswordActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_recover_password)
+    }
+}

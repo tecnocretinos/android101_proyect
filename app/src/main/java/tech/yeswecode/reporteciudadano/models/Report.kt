8f686687bf6f9df0f6326c3fcf84135055cafb60
Tiago Moreno <santiago.moreno@astropay.com>
2022-07-05 18:11:48 -0500

    More mock data

--- app/src/main/java/tech/yeswecode/reporteciudadano/models/Report.kt
+++ app/src/main/java/tech/yeswecode/reporteciudadano/models/Report.kt
@@ -45,6 +45,30 @@ class Report(val id: String,
                     10.0,
                     Date(),
                     ArrayList<String>()
+                ),
+                Report("05",
+                    "Titulo del reporte 5",
+                    "Descripción del reporte con mucho texto pero en esta parte deberían a parecer solo don lineas o incluso más. Aqui deberia hacer un salto de linea y agregar puntos suspensivos. \n Descripción del reporte con mucho texto pero en esta parte deberían a parecer solo don lineas o incluso más. Aqui deberia hacer un salto de linea y agregar puntos suspensivos",
+                    10.0,
+                    10.0,
+                    Date(),
+                    ArrayList<String>()
+                ),
+                Report("06",
+                    "Titulo del reporte 6",
+                    "Descripción del reporte con mucho texto pero en esta parte deberían a parecer solo don lineas o incluso más. Aqui deberia hacer un salto de linea y agregar puntos suspensivos. \n Descripción del reporte con mucho texto pero en esta parte deberían a parecer solo don lineas o incluso más. Aqui deberia hacer un salto de linea y agregar puntos suspensivos",
+                    10.0,
+                    10.0,
+                    Date(),
+                    ArrayList<String>()
+                ),
+                Report("07",
+                    "Titulo del reporte 7",
+                    "Descripción del reporte con mucho texto pero en esta parte deberían a parecer solo don lineas o incluso más. Aqui deberia hacer un salto de linea y agregar puntos suspensivos. \n Descripción del reporte con mucho texto pero en esta parte deberían a parecer solo don lineas o incluso más. Aqui deberia hacer un salto de linea y agregar puntos suspensivos",
+                    10.0,
+                    10.0,
+                    Date(),
+                    ArrayList<String>()
                 )
             )
         }

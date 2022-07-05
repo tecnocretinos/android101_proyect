35c3dad224bf8badf69885529abb26068d486fc9
Tiago Moreno <santiago.moreno@astropay.com>
2022-07-05 17:59:25 -0500

    Reports recyclerview created

--- app/src/main/java/tech/yeswecode/reporteciudadano/models/Report.kt
+++ app/src/main/java/tech/yeswecode/reporteciudadano/models/Report.kt
@@ -10,4 +10,43 @@ class Report(val id: String,
              val latitude: Double,
              val date: Date,
              val images: ArrayList<String>
-)
+) {
+    companion object {
+        fun mock() : Array<Report> {
+            return arrayOf(
+                Report("01",
+                    "Titulo del reporte 1",
+                    "Descripción del reporte con mucho texto pero en esta parte deberían a parecer solo don lineas o incluso más. Aqui deberia hacer un salto de linea y agregar puntos suspensivos",
+                    10.0,
+                    10.0,
+                    Date(),
+                    ArrayList<String>()
+                ),
+                Report("02",
+                    "Titulo del reporte 2",
+                    "Descripción del reporte con mucho texto pero en esta parte deberían a parecer solo don lineas o incluso más. Aqui deberia hacer un salto de linea y agregar puntos suspensivos",
+                    10.0,
+                    10.0,
+                    Date(),
+                    ArrayList<String>()
+                ),
+                Report("03",
+                    "Titulo del reporte 3",
+                    "Descripción del reporte con mucho texto pero en esta parte deberían a parecer solo don lineas o incluso más. Aqui deberia hacer un salto de linea y agregar puntos suspensivos",
+                    10.0,
+                    10.0,
+                    Date(),
+                    ArrayList<String>()
+                ),
+                Report("04",
+                    "Titulo del reporte 4",
+                    "Descripción del reporte con mucho texto pero en esta parte deberían a parecer solo don lineas o incluso más. Aqui deberia hacer un salto de linea y agregar puntos suspensivos",
+                    10.0,
+                    10.0,
+                    Date(),
+                    ArrayList<String>()
+                )
+            )
+        }
+    }
+}
--- app/src/main/java/tech/yeswecode/reporteciudadano/views/HomeActivity.kt
+++ app/src/main/java/tech/yeswecode/reporteciudadano/views/HomeActivity.kt
@@ -4,17 +4,29 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.activity_home.*
 import tech.yeswecode.reporteciudadano.R
+import tech.yeswecode.reporteciudadano.models.Report
 import tech.yeswecode.reporteciudadano.models.User
 import tech.yeswecode.reporteciudadano.utilities.ExtrasConstants
+import tech.yeswecode.reporteciudadano.views.adapters.ReportAdapter
 
 class HomeActivity : AppCompatActivity() {
-
     private var user: User? = null
+    private lateinit var layoutManager: RecyclerView.LayoutManager
+    private lateinit var adapter: ReportAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
         this.user = intent.extras?.getSerializable(ExtrasConstants.USER) as? User
+        this.layoutManager = LinearLayoutManager(this)
+        this.adapter = ReportAdapter(Report.mock())
+
+        reportsRecycler.layoutManager = layoutManager
+        reportsRecycler.adapter = adapter
     }
 }

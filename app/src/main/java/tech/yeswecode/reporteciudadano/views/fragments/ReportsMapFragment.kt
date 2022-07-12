9fdd5bf8d04ec3306f27618d80d74f6601170ab3
Tiago Moreno <santiago.moreno@astropay.com>
2022-07-11 22:29:55 -0500

    Binding map fragment

--- app/src/main/java/tech/yeswecode/reporteciudadano/views/fragments/ReportsMapFragment.kt
+++ app/src/main/java/tech/yeswecode/reporteciudadano/views/fragments/ReportsMapFragment.kt
@@ -14,30 +14,27 @@ import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import tech.yeswecode.reporteciudadano.R
+import tech.yeswecode.reporteciudadano.databinding.FragmentProfileBinding
+import tech.yeswecode.reporteciudadano.databinding.FragmentReportsMapBinding
 
 class ReportsMapFragment : Fragment() {
 
     private val callback = OnMapReadyCallback { googleMap ->
-        /**
-         * Manipulates the map once available.
-         * This callback is triggered when the map is ready to be used.
-         * This is where we can add markers or lines, add listeners or move the camera.
-         * In this case, we just add a marker near Sydney, Australia.
-         * If Google Play services is not installed on the device, the user will be prompted to
-         * install it inside the SupportMapFragment. This method will only be triggered once the
-         * user has installed Google Play services and returned to the app.
-         */
         val sydney = LatLng(-34.0, 151.0)
         googleMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
         googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
     }
+
+    private var _binding: FragmentReportsMapBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_reports_map, container, false)
+        _binding = FragmentReportsMapBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {

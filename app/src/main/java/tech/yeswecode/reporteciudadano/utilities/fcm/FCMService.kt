2ad3132f34e530aab3ca29beb603fcecb8e9cfd0
Tiago Moreno <santiago.moreno@astropay.com>
2022-07-24 17:01:45 -0500

    Parse notification

--- app/src/main/java/tech/yeswecode/reporteciudadano/utilities/fcm/FCMService.kt
+++ app/src/main/java/tech/yeswecode/reporteciudadano/utilities/fcm/FCMService.kt
@@ -2,12 +2,21 @@ package tech.yeswecode.reporteciudadano.utilities.fcm
 
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
+import com.google.gson.Gson
 import tech.yeswecode.reporteciudadano.utilities.NotificationUtil
+
+data class FCMNotification(val id: String = "",
+                           val title: String = "",
+                           val message: String = "")
 
 class FCMService: FirebaseMessagingService() {
 
     override fun onMessageReceived(message: RemoteMessage) {
-        val data = message.data.toString()
-        NotificationUtil.showNotification(this,"Reporte ciudadano", data)
+        val data = message.data
+        val json = Gson().toJson(data)
+        val notification = Gson().fromJson<FCMNotification>(json, FCMNotification::class.java)
+        NotificationUtil.showNotification(this,
+            notification.title,
+            notification.message)
     }
 }

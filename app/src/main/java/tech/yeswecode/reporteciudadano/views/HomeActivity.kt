a66c546aa8901c0e6b6a670ba938a520068933b9
Tiago Moreno <santiago.moreno@astropay.com>
2022-07-28 21:41:59 -0500

    Current pos

--- app/src/main/java/tech/yeswecode/reporteciudadano/views/HomeActivity.kt
+++ app/src/main/java/tech/yeswecode/reporteciudadano/views/HomeActivity.kt
@@ -2,9 +2,12 @@ package tech.yeswecode.reporteciudadano.views
 
 import android.Manifest
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
@@ -26,6 +29,22 @@ class HomeActivity : AppCompatActivity(), ReportSeeMore {
     private lateinit var reportsListFragment: ReportsListFragment
     private lateinit var mapFragment: ReportsMapFragment
     private lateinit var profileFragment: ProfileFragment
+
+    @RequiresApi(Build.VERSION_CODES.N)
+    private val locationPermissionRequest = registerForActivityResult(
+        ActivityResultContracts.RequestMultiplePermissions()
+    ) { permissions ->
+        when {
+            permissions.getOrDefault(Manifest.permission.ACCESS_FINE_LOCATION, false) -> {
+                // TODO: Precise location access granted.
+            }
+            permissions.getOrDefault(Manifest.permission.ACCESS_COARSE_LOCATION, false) -> {
+                // TODO: Only approximate location access granted.
+            } else -> {
+            // TODO: Handle permission rejected
+        }
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -76,9 +95,16 @@ class HomeActivity : AppCompatActivity(), ReportSeeMore {
     }
 
     private fun requestLocationPermission() {
-        ActivityCompat.requestPermissions(this,
-            arrayOf(Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION),
-            REQUEST_PERMISSION)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            locationPermissionRequest.launch(arrayOf(
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION))
+        } else {
+            ActivityCompat.requestPermissions(this,
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION),
+                REQUEST_PERMISSION)
+        }
     }
 
     private fun showFragment(fragment: Fragment) {
--- app/src/main/java/tech/yeswecode/reporteciudadano/views/fragments/NewReportMapFragment.kt
+++ app/src/main/java/tech/yeswecode/reporteciudadano/views/fragments/NewReportMapFragment.kt
@@ -1,5 +1,8 @@
 package tech.yeswecode.reporteciudadano.views.fragments
 
+import android.annotation.SuppressLint
+import android.content.Context.LOCATION_SERVICE
+import android.location.LocationManager
 import androidx.fragment.app.Fragment
 
 import android.os.Bundle
@@ -8,6 +11,7 @@ import android.view.View
 import android.view.ViewGroup
 
 import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
@@ -19,12 +23,10 @@ class NewReportMapFragment : Fragment() {
 
     private var _binding: FragmentNewReportMapBinding? = null
     private val binding get() = _binding!!
+    private lateinit var manager: LocationManager
 
     private val callback = OnMapReadyCallback { googleMap ->
-        // TODO: Place the marker on your position
-        val sydney = LatLng(-34.0, 151.0)
-        googleMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        setInitialPosition(googleMap)
     }
 
     override fun onCreateView(
@@ -33,6 +35,7 @@ class NewReportMapFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         _binding = FragmentNewReportMapBinding.inflate(inflater, container, false)
+        manager = activity?.getSystemService(LOCATION_SERVICE) as LocationManager
         return binding.root
     }
 
@@ -40,6 +43,16 @@ class NewReportMapFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
         mapFragment?.getMapAsync(callback)
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun setInitialPosition(map: GoogleMap) {
+        val pos = manager?.getLastKnownLocation(LocationManager.NETWORK_PROVIDER)
+        pos?.let {
+            val marker = LatLng(it.latitude, it.longitude)
+            map.addMarker(MarkerOptions().position(marker).title("Mi posición"))
+            map.moveCamera(CameraUpdateFactory.newLatLng(marker))
+        }
     }
 
     companion object {
